ee0728e1e882cf748f55c3abfe239318e264f1b5
Rafael Fonseca <rafael.abreu.fonseca@gmail.com>
2024-06-14 15:11:50 +0200

    Adds variables knowledge

---
+++ src/company/cursojava/aula10/variaveis.java
@@ -0,0 +1,14 @@
+package company.cursojava.aula10;
+
+public interface variaveis {
+
+	public static void main(String[] args) {
+		int idade = 32;
+		String nome = "Rafael";
+		String nomeDoMeuCachorro = "Rex";
+		
+		String sentence = nome + " têm " + idade + " anos de idade e um cão chamado " + nomeDoMeuCachorro;
+		System.out.println(sentence);
+	}
+
+}
